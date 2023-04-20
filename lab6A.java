c5dca7e47e9ad84e82e9838651bb55f082851f49
Romario Arnat <Romario.Arnat@green.com.na>
2023-04-20 09:44:14 +0200

    commit message

---
+++ lab6A.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class lab6A {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter start number: ");
+        int startNo = input.nextInt();
+        System.out.print("Enter end number: ");
+        int endNo = input.nextInt();
+
+        if (startNo < 0) {
+            System.out.println("startNo cannot be less than 0");
+            return;
+        }
+
+        if (endNo < 0) {
+            System.out.println("endNo cannot be less than 0");
+            return;
+        }
+
+        if (endNo > 9) {
+            System.out.println("endNo cannot be greater than 9");
+            return;
+        }
+
+        if (startNo > endNo) {
+            System.out.println("startNo must NOT be greater than endNo");
+            return;
+        }
+
+        int sumCube = 0;
+        for (int i = startNo; i <= endNo; i++) {
+            sumCube += i * i * i;
+        }
+
+        System.out.println("Sum of Cube of Numbers = " + sumCube);
+    }
+}
